7013b610cb1ca8370631dc13e41d5a9847aa3728
PatrikBorzik2426 <xborzik@stuba.sk>
2024-10-03 22:33:20 +0200

    Login and Register form

---
+++ vpwa_bejtic_borzik/src/pages/MainLogin.vue
@@ -0,0 +1,57 @@
+<template>
+    <q-page :style-fn="myTweak" padding class="fullscreen flex column flex-center">
+      <h2>Welcome in <spam class=" text-bold text-primary">Comb-bot</spam></h2>
+      <q-form
+        @submit.prevent="handleSubmit"
+        @reset="handleSubmit"
+        class="q-gutter-lg q-ma-lg flex column flex-center no-margin q-pa-xl"
+      >
+      
+      <h1 class=" text-h4 q-ml-none q-mt-none q-mb-lg">Login Form</h1>
+
+      <q-input v-model="login" :rules="[requiredRule]" :error="!!loginError" :error-message="loginError" type="text" label="Login" placeholder="example: YourLogin" class="no-margin"/>
+      <q-input v-model="password" :rules="[requiredRule]" :error="!!loginError" :error-message="passwordError" type="text" label="Password" placeholder="example: YourPassword" class="no-margin"/>
+    
+        
+      <div class=" button-div flex flex-center">
+        <q-btn label="Submit" type="submit" color="primary" class=""/>
+        <q-btn label="Reset" type="cancel" color="primary" flat class="" />
+      </div>
+      
+      <router-link to="/register" class=" q-ml-none text-primary">Go to Registration Page</router-link>
+
+      </q-form>
+    </q-page>
+  </template>
+  
+<script setup lang="ts">
+import { ref } from 'vue'
+const login = ref<string>('')
+const password = ref<string>('')
+
+const loginError = ref<string>('')
+const passwordError = ref<string>('')
+
+const requiredRule = (value: string) => !!value || 'This field is required.'
+
+function handleSubmit(){
+  if (!login.value) {
+    loginError.value = 'This field is required.'
+  }
+  if (!password.value) {
+    passwordError.value = 'This field is required.'
+  }
+}
+
+
+function myTweak() {
+  return { backgroundColor: 'transparent' }
+
+}
+
+defineOptions({
+  name: 'MainLogin'
+});
+
+</script>
+  
---
+++ vpwa_bejtic_borzik/src/pages/MainRegister.vue
@@ -0,0 +1,80 @@
+<template>
+<q-page :style-fn="myTweak" padding class="fullscreen flex flex-center">
+  <div class=" q-mr-xl" style="max-width: 35%;">
+    <h2 class="text-center" style="width: fit-content; margin: 0 auto;">
+      Welcome in 
+    </h2>
+    <h2 class="text-bold text-primary text-center" style="width: fit-content; margin: 0 auto;">Comb-bot</h2>
+    <p class=" text-h5 q-mt-xl" style="text-align: center; width: fit-content;">
+      Our comb is your comb as well, where you can meet your friends, family and colleagues.<br>
+      The biggest advantage of our comb is that you can meet people from all over the world. 
+    </p>
+  </div>
+
+  <q-form
+    @submit.prevent="handleSubmit"
+    @reset="handleSubmit"
+    class="q-gutter-lg q-ma-lg flex column flex-center no-margin q-pa-xl"
+  >
+    <h1 class="text-h4 q-ml-none q-mt-none q-mb-lg">Register Form</h1>
+
+
+    <q-input v-model="firstName" :rules="[requiredRule]" :error="!!loginError" :error-message="firstNameError" type="text" label="First Name" placeholder="example: Joe" class="no-margin"/>
+    <q-input v-model="lastName" :rules="[requiredRule]" :error="!!loginError" :error-message="lastNameError" type="text" label="Last Name" placeholder="example: Doe" class="no-margin"/>
+    <q-input v-model="email" :rules="[requiredRule]" :error="!!loginError" :error-message="emailError" type="text" label="E-mail" placeholder="example: joe.deo@domain.eu" class="no-margin"/>
+    <q-input v-model="login" :rules="[requiredRule]" :error="!!loginError" :error-message="loginError" type="text" label="Login" placeholder="example: YourLogin" class="no-margin"/>
+    <q-input v-model="password" :rules="[requiredRule]" :error="!!loginError" :error-message="passwordError" type="text" label="Password" placeholder="example: YourPassword" class="no-margin"/>
+    
+    <div class="button-div flex flex-center">
+      <q-btn label="Submit" type="submit" color="primary"/>
+      <q-btn label="Reset" type="cancel" color="primary" flat/>
+    </div>
+    
+    <router-link to="/" class="q-ml-none text-primary">I am already logged-in</router-link>
+  </q-form>
+</q-page>
+
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+const login = ref<string>('')
+const password = ref<string>('')
+const firstName = ref<string>('')
+const lastName = ref<string>('')
+const email = ref<string>('')
+
+const loginError = ref<string>('')
+const passwordError = ref<string>('')
+const firstNameError = ref<string>('')
+const lastNameError = ref<string>('')
+const emailError = ref<string>('')
+
+const requiredRule = (value: string) => !!value || 'This field is required.'
+
+function handleSubmit(){
+  if (!login.value) {
+    loginError.value = 'This field is required.'
+  }
+  if (!password.value) {
+    passwordError.value = 'This field is required.'
+  }
+  if (!firstName.value) {
+    firstNameError.value = 'This field is required.'
+  }
+  if (!lastName.value) {
+    lastNameError.value = 'This field is required.'
+  }
+  if (!email.value) {
+    emailError.value = 'This field is required.'
+  }
+}
+
+
+function myTweak() { return { backgroundColor: 'transparent' } }
+
+defineOptions({
+name: 'MainLogin'
+});
+
+</script>
