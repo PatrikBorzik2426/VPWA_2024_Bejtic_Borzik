1680e2ea83c3341d7dac879d6e0352dc8fc4004c
ViktorBejtic <bejtic.com@gmail.com>
2024-10-18 00:10:54 +0200

    sidepanel

---
+++ vpwa_bejtic_borzik/src/components/SidePanel.vue
@@ -0,0 +1,53 @@
+<template>
+    <q-page>
+      <q-drawer 
+      show-if-above 
+      v-model="leftDrawerOpen" 
+      elevated
+      mini
+      side="left" 
+      class="q-pa-md q-ma-none q-rounded-both bg-grey-10" 
+      content-class="q-pa-none q-rounded-both"
+
+      >
+        <q-list>
+          <!-- Radial Button for Profile -->
+          <q-item>
+            <q-btn round icon="account_circle" color="grey-9"  />
+          </q-item>
+  
+          <!-- Radial Button for Friends -->
+          <q-item>
+            <q-btn round elevated icon="group" color="grey-9"  />
+          </q-item>
+  
+          <!-- Radial Button for Servers -->
+          <q-item>
+            <q-btn round elevated icon="dns" color="grey-9"/>
+          </q-item>
+        </q-list>
+      </q-drawer>
+    </q-page>
+  </template>
+
+
+
+<script setup lang="ts">
+import { ref } from 'vue'
+const leftDrawerOpen = ref(true)
+
+</script>
+
+
+
+<style scoped>
+
+.q-drawer{
+    padding: 10px !important;
+    max-height: 100vh !important;
+    overflow: auto !important;
+    border-top-right-radius: 15px !important;
+    border-bottom-right-radius: 15px !important;
+}
+
+</style>
--- vpwa_bejtic_borzik/src/pages/ChatApp.vue
+++ vpwa_bejtic_borzik/src/pages/ChatApp.vue
@@ -1,12 +1,18 @@
 <template>
-  <q-page padding>
-    <!-- content -->
+  <q-page>
+    <SidePanel />
+
   </q-page>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue'
-export default defineComponent({
-  // name: 'PageName'
-})
+<script setup lang="ts">
+import SidePanel from '../components/SidePanel.vue'
+
+
 </script>
+
+<style scoped>
+  .q-page {
+    background-color: var(--q-dark);
+  }
+</style>
